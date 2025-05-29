e19b7fd56d68f346089ad12e961ee97125c18511
JSA2002 <110814196+JSA2002@users.noreply.github.com>
2025-05-29 22:15:44 +0530

    Update Sidebar.jsx

--- healthcare-dashboard/src/components/Sidebar/Sidebar.jsx
+++ healthcare-dashboard/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 // src/components/Sidebar/Sidebar.jsx
 import React from 'react';
-import './Sidebar.css';
+import './sidebar.css';
 import { navigationLinks } from '../../data/navigationLinks';
 
 export default function Sidebar() {
